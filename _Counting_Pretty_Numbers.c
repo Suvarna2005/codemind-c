7999ed811342a80582281df630d71da19237a39f
Suvarna2005 <121661235+Suvarna2005@users.noreply.github.com>
2023-02-05 16:57:13 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d",&n);     while(n--)     {         int a,b,c=0;         scanf("%d%d",&a,&b);         for(i=a;i<=b;i++)         {             if(i%10==2||i%10==3||i%10==9)             c++;         }         printf("%d ",c);     } }

---
+++ _Counting_Pretty_Numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    while(n--)
+    {
+        int a,b,c=0;
+        scanf("%d%d",&a,&b);
+        for(i=a;i<=b;i++)
+        {
+            if(i%10==2||i%10==3||i%10==9)
+            c++;
+        }
+        printf("%d
+",c);
+    }
+}
